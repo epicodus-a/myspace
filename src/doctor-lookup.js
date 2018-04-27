a6f7db7ad918e1a75bec0aaf020a728eff001fcc
Adrianacmy <adrianamullaney@gmail.com>
2018-04-27 13:02:36 -0700

    create method getGeometry

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -14,8 +14,8 @@
 
 
 
-class Doctor{
-  constructor(firstName, lastName, address, phoneNumber, website, acceptPatient, title, image, bio,ratings, specialties){
+class Doctor {
+  constructor(firstName, lastName, address, phoneNumber, website, acceptPatient, title, image, bio, ratings, specialties) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.address = address;
@@ -29,15 +29,15 @@ class Doctor{
     this.specialties = specialties;
   }
 
-  getDoctorsByMedicalIssue(key, query, location){
-    return new Promise(function(resolve, reject){
+  getDoctorsByMedicalIssue(key, query, location) {
+    return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=3&query=${query}`;
 
-      request.onload = function(){
-        if(this.status === 200){
+      request.onload = function () {
+        if (this.status === 200) {
           resolve(request.response);
-        }else{
+        } else {
           reject(Error(request.statusText));
         }
       }
@@ -45,18 +45,18 @@ class Doctor{
       request.open('GET', url, true);
       request.send();
     });
-    
+
   }
 
-  getDoctorsByName(key, name, location){
-    return new Promise(function(resolve, reject){
+  getDoctorsByName(key, name, location) {
+    return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=3&query=${name}`;
 
-      request.onload = function(){
-        if(this.status === 200){
+      request.onload = function () {
+        if (this.status === 200) {
           resolve(request.response);
-        }else{
+        } else {
           reject(Error(request.statusText));
         }
       }
@@ -67,21 +67,39 @@ class Doctor{
 
   }
 
-  doctorMeta(doc){
-    if(doc.length >= 1){
-     this.acceptPatient = doc.practices.accepts_new_patients;
-     this.address = `${doc.practices.visit_address.street}, ${doc.practices.visit_address.city}, ${doc.practices.visit_address.state}, ${doc.practices.visit_address.zip}`;
-     this.phoneNumber = `${doc.practices.phones.type}: ${doc.practices.phones.phoneNumber}`;
-     this.firstName = doc.profile.first_name;
-     this.lastName = doc.profile.last_name;
-     this.title = doc.profile.title;
-     this.image = doc.profile.image_url;
-     this.bio = doc.profile.bio;
-     this.ratings = doc.ratings;
-     this.specialties = doc.specialties.name;
-    }else{
+  doctorMeta(doc) {
+    if (doc.length >= 1) {
+      this.acceptPatient = doc.practices.accepts_new_patients;
+      this.address = `${doc.practices.visit_address.street}, ${doc.practices.visit_address.city}, ${doc.practices.visit_address.state}, ${doc.practices.visit_address.zip}`;
+      this.phoneNumber = `${doc.practices.phones.type}: ${doc.practices.phones.phoneNumber}`;
+      this.firstName = doc.profile.first_name;
+      this.lastName = doc.profile.last_name;
+      this.title = doc.profile.title;
+      this.image = doc.profile.image_url;
+      this.bio = doc.profile.bio;
+      this.ratings = doc.ratings;
+      this.specialties = doc.specialties.name;
+    } else {
       return 'Your doctor went to Marse.';
     }
+  }
+
+  getGeometry(address, googleApiKey, format) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=3&query=${query}`;
+      let url = `https://maps.googleapis.com/maps/api/geocode/${format}?address=${address}&key=${googleApiKey}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+
+      request.open('GET', url, true);
+      request.send();
+    });
   }
 
 }
