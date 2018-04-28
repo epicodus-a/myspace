9b51783b883603b968dc200b984e20a70a9fce4d
Adrianacmy <adrianamullaney@gmail.com>
2018-04-27 18:14:11 -0700

    search results are not accurate

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,7 +1,7 @@
 let Promise = require('es6-promise').Promise;
 
 export class Doctor {
-  constructor(firstName='', lastName='', address='', phoneNumber='', website='', acceptPatient=true, title='', image='', bio='', ratings='', specialties='') {
+  constructor(firstName='', lastName='', address='portland, oregon', phoneNumber='', website='', acceptPatient=true, title='', image='', bio='', ratings='', specialties='') {
     this.firstName = firstName;
     this.lastName = lastName;
     this.address = address;
@@ -15,17 +15,17 @@ export class Doctor {
     this.specialties = specialties;
   }
 
-  getDoctorsByMedicalIssue(key, issue, location, name, specialty) {
+  getDoctors(key, issue, location, name, specialty) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
       
-      let url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=3&query=${issue}`;
-      if(Boolean(name)){
-        url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=3&query=${issue}&name=${name}`;
-      }else if(Boolean(specialty)){
-        url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=3&query=${issue}&specialty_uid=${specialty}`;
-      }else if(Boolean(name) && Boolean(specialty)){
-        url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=3&query=${issue}&name=${name}&specialty_uid=${specialty_uid}`;
+      let url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=8&query=${issue}`;
+      if(name){
+        url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=2&query=${issue}&name=${name}`;
+      }else if(specialty){
+        url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=2&query=${issue}&specialty_uid=${specialty}`;
+      }else if(name && specialty){
+        url = `https://api.betterdoctor.com/2018-03-01/doctors?user_location=${location}&user_key=${key}&limit=2&query=${issue}&name=${name}&specialty_uid=${specialty}`;
       }
       request.onload = function () {
         if (this.status === 200) {
@@ -34,7 +34,6 @@ export class Doctor {
           reject(Error(request.statusText));
         }
       }
-
       request.open('GET', url, true);
       request.send();
     });
@@ -42,27 +41,27 @@ export class Doctor {
   }
 
 
-  doctorMeta(doc) {
-    if (doc.length >= 1) {
-      this.acceptPatient = doc.practices.accepts_new_patients;
-      this.address = `${doc.practices.visit_address.street}, ${doc.practices.visit_address.city}, ${doc.practices.visit_address.state}, ${doc.practices.visit_address.zip}`;
-      this.phoneNumber = `${doc.practices.phones.type}: ${doc.practices.phones.phoneNumber}`;
-      this.firstName = doc.profile.first_name;
-      this.lastName = doc.profile.last_name;
-      this.title = doc.profile.title;
-      this.image = doc.profile.image_url;
-      this.bio = doc.profile.bio;
-      this.ratings = doc.ratings;
-      this.specialties = doc.specialties.uid;
-    } else {
-      return 'Your doctor went to Marse.';
-    }
-  }
+  // doctorMeta(doc) {
+  //   if (doc.length >= 1) {
+  //     this.acceptPatient = doc.practices.accepts_new_patients;
+  //     this.address = `${doc.practices.visit_address.street}, ${doc.practices.visit_address.city}, ${doc.practices.visit_address.state}, ${doc.practices.visit_address.zip}`;
+  //     this.phoneNumber = `${doc.practices.phones.type}: ${doc.practices.phones.phoneNumber}`;
+  //     this.firstName = doc.profile.first_name;
+  //     this.lastName = doc.profile.last_name;
+  //     this.title = doc.profile.title;
+  //     this.image = doc.profile.image_url;
+  //     this.bio = doc.profile.bio;
+  //     this.ratings = doc.ratings;
+  //     this.specialties = doc.specialties;
+  //   } else {
+  //     return 'Your doctor went to Marse.';
+  //   }
+  // }
 
-  getGeometry(address, googleApiKey, format) {
+  getGeometry(address, googleApiKey) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://maps.googleapis.com/maps/api/geocode/${format}?address=${address}&key=${googleApiKey}`;
+      let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${googleApiKey}`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -80,7 +79,7 @@ export class Doctor {
   getSpecialties(key) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2018-03-01/specialties?user_key=${key}&limit=5`;
+      let url = `https://api.betterdoctor.com/2018-03-01/specialties?user_key=${key}&limit=10`;
 
       request.onload = function () {
         if (this.status === 200) {
